32b64218bab24250cf8b09813195ad2ccc9d1439
LambertPaulo <lambertpaulo.manalo@synlab.com>
2022-07-29 12:39:05 +0800

    Creation of Simple In Memory Identity Server

---
+++ IdentityServer/Program.cs
@@ -0,0 +1,27 @@
+using Duende.IdentityServer.Models;
+using Duende.IdentityServer.Test;
+using Extensions;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddIdentityServer(option => 
+{
+    option.Events.RaiseErrorEvents = true;
+    option.Events.RaiseInformationEvents = true;
+    option.Events.RaiseFailureEvents = true;
+    option.Events.RaiseSuccessEvents = true;
+
+    option.EmitStaticAudienceClaim = true;
+})
+.AddTestUsers(new List<TestUser>())
+.AddInMemoryClients(Config.Clients)
+.AddInMemoryApiResources(Config.ApiResources)
+.AddInMemoryApiScopes(Config.ApiScopes)
+.AddInMemoryIdentityResources(Config.IdentityResources);
+
+var app = builder.Build();
+app.UseIdentityServer();
+
+app.MapGet("/", () => "Hello World!");
+
+app.Run();
