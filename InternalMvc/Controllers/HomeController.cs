8ff4eb9542411f6afe9f8b80a88642b1c41efa79
LambertPaulo <lambertpaulo.manalo@synlab.com>
2022-08-02 01:49:37 +0800

    Added MVC Client

---
+++ InternalMvc/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using InternalMvc.Models;
+using Newtonsoft.Json;
+using WeatherMVC.Models;
+
+namespace InternalMvc.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    public async Task<IActionResult> Weather()
+    {
+        using var client = new HttpClient();
+
+        var result = await client.GetAsync("https://localhost:5445/weatherforecast");
+
+        if(result.IsSuccessStatusCode)
+        {
+            var model = await result.Content.ReadAsStringAsync();
+
+            var data = JsonConvert.DeserializeObject<List<WeatherData>>(model);
+
+            return View(data);
+        }
+
+        throw new Exception("Unable to get content");
+    }    
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
---
+++ InternalMvc/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InternalMvc.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
